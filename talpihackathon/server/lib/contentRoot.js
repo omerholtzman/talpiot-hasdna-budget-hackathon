3074988a3d9e47299f8b0956ca985fe57388f2c1
Omer H <omer.holtzman@gmail.com>
2026-07-31 09:08:42 +0300

    rip intial frontend for presenting the csv

--- talpihackathon/server/lib/contentRoot.js
+++
@@ -1,6 +0,0 @@
-import { fileURLToPath } from 'node:url'
-import path from 'node:path'
-
-const thisDir = path.dirname(fileURLToPath(import.meta.url))
-
-export const CONTENT_ROOT = path.resolve(thisDir, '..', '..', 'content')
